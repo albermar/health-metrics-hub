cd2b043696a38fdbd4249655e6f0837bfc506f80
Alberto Bermejillo Martín-Romo <alberto.bmoi@gmail.com>
2026-02-11 19:03:31 +0100

    Add CORS and prepare for deployment

--- app/api/main.py
+++ app/api/main.py
@@ -8,6 +8,16 @@ load_dotenv()
 
 app = FastAPI(title="Health Metrics Hub API", version="1.0.0", debug=True)
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tighten later to your streamlit URL
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #Include routers
 app.include_router(kpis.router, prefix="/api", tags=["KPIs"])
 app.include_router(upload.router, prefix="/api", tags=["Upload"])
